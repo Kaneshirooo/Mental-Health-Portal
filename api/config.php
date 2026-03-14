39f40d19f88ae91015aefc09caabcaeb1acafade
Kaneshirooo <aquinorenz69@gmail.com>
2026-03-15 00:13:18 +0800

    Add database host debug info

--- api/config.php
+++ api/config.php
@@ -26,7 +26,8 @@ mysqli_report(MYSQLI_REPORT_OFF);
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
-    die("Database Connection Error: " . $conn->connect_error);
+    $host_used = DB_HOST ?: '(empty)';
+    die("Database Connection Error (Host: $host_used): " . $conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
